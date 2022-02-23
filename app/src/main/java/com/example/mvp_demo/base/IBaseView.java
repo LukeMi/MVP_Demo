8241bc4be129cdd48a2e1668fd8ae5eaf4d559c7
jeferry <826861729@qq.com>
2022-02-24 06:38:42 +0800

    todo

--- app/src/main/java/com/example/mvp_demo/base/IBaseView.java
+++
@@ -1,7 +0,0 @@
-package com.example.mvp_demo.base;
-
-import androidx.annotation.Nullable;
-
-public interface IBaseView<Presenter extends IPresenter<? extends IBaseView<? extends Presenter>>> {
-    void onNetException(int i, @Nullable String str);
-}
